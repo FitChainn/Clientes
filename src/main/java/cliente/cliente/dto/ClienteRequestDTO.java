bf14ed889b9eaf70c8d50aed559660d640d623d7
VI-Alumno <VI-Alumno@duoc.acad>
2026-05-14 11:52:54 -0400

    test

--- src/main/java/cliente/cliente/dto/ClienteRequestDTO.java
+++ src/main/java/cliente/cliente/dto/ClienteRequestDTO.java
@@ -2,11 +2,13 @@ package cliente.cliente.dto;
 
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.sql.Date;
+import java.time.LocalDate;
 
 @Data
 @NoArgsConstructor
@@ -19,7 +21,7 @@ public class ClienteRequestDTO {
     @NotBlank(message = "El run no puede estar vacio")
     private String run;
 
-    @NotBlank(message = "La fecha no puede esta vacia")
-    private Date fechaNacimiento;
+    @NotNull(message = "La fecha no puede esta vacia")
+    private LocalDate fechaNacimiento;
 
 }
