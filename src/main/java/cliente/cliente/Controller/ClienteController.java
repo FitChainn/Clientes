db15ac4ae2e294bed6483952d1496784b7a45d6c
Crizz12 <cri.cifuentesc@duocuc.cl>
2026-05-20 02:37:06 -0400

    se conecto con micro servicio seguridad

--- src/main/java/cliente/cliente/Controller/ClienteController.java
+++ src/main/java/cliente/cliente/Controller/ClienteController.java
@@ -10,10 +10,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.HashMap;
-
-
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -22,39 +22,39 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("v1/clientes")
 public class ClienteController {
+
     @Autowired
     private ClienteService clienteService;
 
-
+    @PreAuthorize("hasAnyRole('ADMIN', 'ENTRENADOR')")
     @GetMapping
-    public ResponseEntity <List<ClienteResponseDTO>> obtenerClientes(){
+    public ResponseEntity<List<ClienteResponseDTO>> obtenerClientes() {
         List<ClienteResponseDTO> clientes = clienteService.obtenerClientes();
-        if(clientes.isEmpty()){
-            return ResponseEntity.noContent().build();
-        }
+        if (clientes.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(clientes);
-
     }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'ENTRENADOR', 'CLIENTE')")
     @GetMapping("/{id}")
-    public ResponseEntity<ClienteResponseDTO> obtenerCliente (@PathVariable Long id){
-        return clienteService.obtenerCliente(id).
-                map(ResponseEntity::ok)
+    public ResponseEntity<ClienteResponseDTO> obtenerCliente(@PathVariable Long id) {
+        return clienteService.obtenerCliente(id)
+                .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<ClienteResponseDTO> RegistrarCliente(@Valid @RequestBody ClienteRequestDTO nuevo)
-    {
+    public ResponseEntity<ClienteResponseDTO> RegistrarCliente(@Valid @RequestBody ClienteRequestDTO nuevo) {
         return ResponseEntity.status(201).body(clienteService.saveCliente(nuevo));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("{id}")
-    public ResponseEntity<?> eliminar (@PathVariable Long id){
-        if(clienteService.obtenerCliente(id).isEmpty()){
+    public ResponseEntity<?> eliminar(@PathVariable Long id) {
+        if (clienteService.obtenerCliente(id).isEmpty()) {
             Map<String, String> mensaje1 = new HashMap<>();
             mensaje1.put("mensaje", "Cliente no encontrado");
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(mensaje1);
-
         }
         clienteService.eliminarPorId(id);
         log.info("Cliente eliminado");
@@ -63,20 +63,23 @@ public class ClienteController {
         return ResponseEntity.ok(mensaje);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{clienteId}/entrenador/{entrenadorId}")
     public ResponseEntity<ClienteResponseDTO> asignarEntrenador(
             @PathVariable Long clienteId,
             @PathVariable Long entrenadorId) {
         return ResponseEntity.ok(clienteService.asignarEntrenador(clienteId, entrenadorId));
     }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'ENTRENADOR')")
     @GetMapping("/entrenador/{entrenadorId}")
     public ResponseEntity<List<ClienteResponseDTO>> obtenerPorEntrenador(@PathVariable Long entrenadorId) {
         List<ClienteResponseDTO> clientes = clienteService.obtenerClientesPorEntrenador(entrenadorId);
-        if (clientes.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
+        if (clientes.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(clientes);
     }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'ENTRENADOR')")
     @GetMapping("/entrenador/{entrenadorId}/simple")
     public ResponseEntity<List<ClienteEntrenador>> obtenerNombresPorEntrenador(@PathVariable Long entrenadorId) {
         List<ClienteEntrenador> clientes = clienteService.obtenerClientesPorEntrenador(entrenadorId)
@@ -85,14 +88,13 @@ public class ClienteController {
                 .collect(Collectors.toList());
         return ResponseEntity.ok(clientes);
     }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'ENTRENADOR')")
     @GetMapping("/establecimiento/{establecimientoId}")
     public ResponseEntity<List<ClienteResponseDTO>> obtenerPorEstablecimiento(
             @PathVariable Long establecimientoId) {
         List<ClienteResponseDTO> clientes = clienteService.obtenerPorEstablecimiento(establecimientoId);
-        if (clientes.isEmpty()) {
-            return ResponseEntity.noContent().build();
-        }
+        if (clientes.isEmpty()) return ResponseEntity.noContent().build();
         return ResponseEntity.ok(clientes);
     }
-
 }
